c80800a3ee6e61c1e0ed032d303fd756335870ff
Olcan <olcans@gmail.com>
2025-04-20 14:49:02 -0700

    use /sandbox/<proj_name> instead of /project as workdir in container (#64)

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,6 +2,7 @@
 
 IMAGE=gemini-code-sandbox
 CLI_DIST=/usr/local/share/npm-global/lib/node_modules/\@gemini-code/cli
+WORKDIR=/sandbox/$(basename "$PWD")
 
 # use docker if installed, otherwise try to use podman instead
 if command -v docker &> /dev/null; then
@@ -13,4 +14,4 @@ else
     exit 1
 fi
 
-$CMD run -it --rm -v"$PWD:/project" --workdir /project "$IMAGE" node "$CLI_DIST"
+$CMD run -it --rm -v"$PWD:$WORKDIR" --workdir "$WORKDIR" "$IMAGE" node "$CLI_DIST"
