64910527deb760087eec26a85ef87adea403b167
Brandon Keiji <brandonkeiji@google.com>
2025-04-28 16:26:46 +0000

    refactor: remove node_modules reference in start command (#198)

--- scripts/start.sh
+++ scripts/start.sh
@@ -26,8 +26,8 @@ else
     # DEV=true to enable React Dev Tools (https://github.com/vadimdemedes/ink?tab=readme-ov-file#using-react-devtools)
     # CLI_VERSION to display in the app ui footer
     if [ -n "${DEBUG:-}" ]; then
-        CLI_VERSION='development' DEV=true node --inspect-brk node_modules/@gemini-code/cli "$@"
+        CLI_VERSION='development' DEV=true npm run debug --workspace=@gemini-code/cli -- "$@"
     else
-        CLI_VERSION='development' DEV=true node node_modules/@gemini-code/cli "$@"
+        CLI_VERSION='development' DEV=true npm run start --workspace=@gemini-code/cli -- "$@"
     fi
 fi
