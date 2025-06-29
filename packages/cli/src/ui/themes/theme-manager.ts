19a9b50aab48394c579d885390438547a93044d2
Ahmad Awais ⌘ <me@AhmadAwais.com>
2025-06-28 20:43:17 -0700

    📦 NEW: Theme Shades of Purple  (#2114)
    Co-authored-by: Scott Densmore <scottdensmore@mac.com>

--- packages/cli/src/ui/themes/theme-manager.ts
+++ packages/cli/src/ui/themes/theme-manager.ts
@@ -13,6 +13,7 @@ import { GitHubLight } from './github-light.js';
 import { GoogleCode } from './googlecode.js';
 import { DefaultLight } from './default-light.js';
 import { DefaultDark } from './default.js';
+import { ShadesOfPurple } from './shades-of-purple.js';
 import { XCode } from './xcode.js';
 import { Theme, ThemeType } from './theme.js';
 import { ANSI } from './ansi.js';
@@ -42,6 +43,7 @@ class ThemeManager {
       GitHubDark,
       GitHubLight,
       GoogleCode,
+      ShadesOfPurple,
       XCode,
       ANSI,
       ANSILight,
